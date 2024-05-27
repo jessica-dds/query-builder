3646bb834f4c15c37636ab2cebc1c22798331f75
jessica-dds <jessica.gleizer@hotmail.com>
2024-05-27 10:55:18 -0300

    Final

--- index.js
+++ index.js
@@ -56,4 +56,38 @@ app.delete('/:id', async (req, res) => {
     const agendaExcluida = await knex('agenda').del().where({ id }).returning('id');
     return res.json(agendaExcluida);
 });
+
+// incluir anotações
+app.post('/:id/anotacoes', async (req, res) => {
+    const { id } = req.params;
+    const { nota } = req.body;
+
+    const anotacao = await knex('anotacoes')
+        .insert({
+            agenda_id: id,
+            nota
+        })
+        .returning('*');
+    return res.json(anotacao);
+});
+
+// Join - agrupar registros 
+app.get('/anotacoes', async (req, res) => {
+    // const anotacoes = await knex('anotacoes')
+    //     .join('agenda', 'anotacoes.agenda_id', 'agenda.id')
+    //     .select('anotacoes.*', 'agenda.nome');
+    const anotacoes = await knex('anotacoes')
+        // left join
+        .leftJoin('agenda', 'anotacoes.agenda_id', 'agenda.id')
+        .select('anotacoes.*', 'agenda.nome');
+    // right join
+    // const anotacoes = await knex('anotacoes')
+    //     .rightJoin('agenda', 'anotacoes.agenda_id', 'agenda.id')
+    //     .select('anotacoes.*', 'agenda.nome');
+    return res.json(anotacoes);
+});
+
+
+
+
 app.listen(3000);
